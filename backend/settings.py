dc17d2c156b8654056ef44defa4fb3c1978743d7
Dennis Rong <rong.dennis@gmail.com>
2013-11-24 17:14:40 -0800

    Added Backend

---
+++ backend/settings.py
@@ -0,0 +1,10 @@
+"""
+Settings for application
+"""
+
+import os
+
+ROOT = os.path.abspath(os.path.dirname(__file__))
+
+# SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(ROOT, 'app.db')
+# SQLALCHEMY_MIGRATE_REPO = os.path.join(ROOT, 'db_repository')
